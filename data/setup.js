87f62cf55ece49fcc2985b14f63fdae8866f14fe
Estefani <76499574+HonduranCoder@users.noreply.github.com>
2021-11-15 11:57:55 -0600

    Update setup.js

--- data/setup.js
+++ data/setup.js
@@ -2,7 +2,6 @@ const fs = require('fs').promises;
 
 module.exports = (pool) => {
   return fs
-  
     .readFile(`${__dirname}/../sql/setup.sql`, { encoding: 'utf-8' })
     .then((sql) => pool.query(sql));
 };
